ad350f942ad5ba2ee21d7c6ecc8d83a8c0b14cba
Aryan-Kr-Yadav <aryanyadav30705@gmail.com>
2025-08-29 15:50:18 +0530

    new note

--- case.py
+++ case.py
@@ -1,9 +1,3 @@
 x,y,z="orange","banana","mango"
 print(id(x),id(y),id(z))
 
-var1= 1
-print(type(var1))
-var2=20.55
-print(type(var2))
-var3="hello"
-print(type(var3))
---
+++ typecheck.py
@@ -0,0 +1,6 @@
+var1= 1
+print(type(var1))
+var2=20.55
+print(type(var2))
+var3="hello"
+print(type(var3))
